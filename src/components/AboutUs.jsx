603fefee2615af34fb9fe5f2e8e96a56c5b7c48f
Anu <anuskhka@123>
2024-01-16 00:49:44 +0530

    second commit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import anu from "../../public/assets/myImg";
 
 const AboutUs = () => {
   return (
@@ -7,20 +8,21 @@ const AboutUs = () => {
         <div className="absolute top-0 left-0 w-full h-full bg-white opacity-40"></div>
         <div className="relative z-10 gap-5 items-center lg:flex">
           <div className="flex-1 max-w-lg py-5 sm:mx-auto sm:text-center lg:max-w-max lg:text-left">
-            <h3 className="text-3xl text-gray-800 font-semibold md:text-4xl">
-              build your websites with{" "}
-              <span className="text-indigo-600">high performance</span>
+            <h3 className="text-xl text-gray-800  md:text-4xl">
+              <span className="text-indigo-600 font-bold text-6xl">
+                Anushka Kishor
+              </span>
+              <br />
+              <p className=" font-semibold text-xl">
+                From Dreams to Digital Realities
+              </p>{" "}
             </h3>
-            <p className="text-gray-500 leading-relaxed mt-3">
-              Nam erat risus, sodales sit amet lobortis ut, finibus eget metus.
-              Cras aliquam ante ut tortor posuere feugiat. Duis sodales nisi id
-              porta lacinia.
-            </p>
+
             <a
               className="mt-5 px-4 py-2 text-indigo-600 font-medium bg-indigo-50 rounded-full inline-flex items-center"
               href="javascript:void()"
             >
-              Try it out
+              Explore
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6 ml-1 duration-150"
@@ -38,11 +40,7 @@ const AboutUs = () => {
             </a>
           </div>
           <div className="flex-1 mt-5 mx-auto sm:w-9/12 lg:mt-0 lg:w-auto">
-            <img
-              src="https://i.postimg.cc/kgd4WhyS/container.png"
-              alt=""
-              className="w-full"
-            />
+            <img src={anu} alt="" className="w-full" />
           </div>
         </div>
       </section>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,21 @@ import React from "react";
 const NavBar = () => {
   return (
     <>
-      <nav className="flex gap-8 w-full h-auto p-6 bg-slate-400">
-        <p className="font-bold text-xl">Anushka</p>
-        <p>About Us</p>
-        <p>Contact us</p>
+      <nav className="flex  w-full h-auto p-6 items-center sticky justify-between px-36 bg-slate-400">
+        <div className="flex gap-8 items-center ">
+          <p className="font-bold text-2xl mr-10 ">Anushka</p>
+          <p>Home</p>
+          <p>About</p>
+          <p>Contact</p>
+          <p>Projects</p>
+        </div>
+
+        <div className="right gap-8 flex text-2xl">
+          <i class="fa-brands fa-linkedin"></i>
+          <i class="fa-brands fa-github"></i>   
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
       </nav>
     </>
   );
